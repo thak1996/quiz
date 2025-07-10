b2772e3812330eead769282d1b2709dbb52be497
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-07-10 16:21:04 -0300

    Add main layout template

---
+++ resources/views/components/main-layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $pageTitle }}</title>
+    <!-- favicon -->
+    <link rel="icon" href="{{ asset('assets/images/favicon.png') }}" type="image/png">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css') }}">
+    <!-- css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+</head>
+
+<body>
+    <!-- logo -->
+    <x-logo />
+
+    {{ $slot }}
+
+    <!-- footer -->
+    <x-footer />
+
+    <!-- bootstrap -->
+    <script src="{{ asset('bootstrap/bootstrap.bundle.min.js') }}"></script>
+
+</body>
+
+</html>
