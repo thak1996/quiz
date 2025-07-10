7cecf696c6d58b5150dbc7e49a17d6a4f2286eee
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-07-10 16:21:56 -0300

    Add MainLayout component for consistent page structure

---
+++ app/View/Components/MainLayout.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class MainLayout extends Component
+{
+    /**
+     * The page title.
+     *
+     * @var string
+     */
+    public string $pageTitle;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(string $pageTitle)
+    {
+        $this->pageTitle = $pageTitle;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.main-layout');
+    }
+}
